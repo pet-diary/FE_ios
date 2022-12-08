aeedf2129199d4bb3f13cd92923010e17e5e05d0
skdud4659 <skdud4659@gmail.com>
2022-12-08 19:13:50 +0900

    [ing] - main UI

---
+++ DayOfPets/Color/Color.swift
@@ -0,0 +1,12 @@
+//
+//  Color.swift
+//  DayOfPets
+//
+//  Created by Nayeong Kim on 2022/12/08.
+//
+
+import UIKit
+
+extension UIColor {
+    class var mainColor: UIColor? { return UIColor(named: "AccentColor") }
+}
